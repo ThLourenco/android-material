88d601adf482bb73faa31b9c1554f8e65e1b88b8
ThLourenco <thilourenco01@gmail.com>
2025-02-07 23:06:55 -0300

    finalizando estudo text fields

--- app/src/main/java/com/example/android_material/ButtonsActivity.kt
+++ app/src/main/java/com/example/android_material/ButtonsActivity.kt
@@ -1,2 +1,22 @@
-package com.example.android_material 
+package com.example.android_material
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_material.databinding.ActivityButtonsBinding
+
+class ButtonsActivity: AppCompatActivity() {
+
+
+    private val binding by lazy { ActivityButtonsBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    companion object{
+        fun createIntent(context: Context): Intent = Intent(context, ButtonsActivity::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/android_material/TextFieldsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.android_material
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_material.databinding.ActivityTextFieldsBinding
+
+class TextFieldsActivity: AppCompatActivity() {
+
+    private val binding by lazy { ActivityTextFieldsBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.imputErro.error = "Campo obrigatorio"
+
+    }
+
+    companion object{
+        fun createIntent(context: Context): Intent = Intent(context, ActivityTextFieldsBinding::class.java)
+    }
+
+
+
+}
